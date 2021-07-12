a1b13a294baef504a1fb7af59c32304fd284eb8a
xun415 <xun415@naver.com>
2021-07-12 22:48:23 +0900

    init

---
+++ src/main/java/com/example/SpringwebProject/SpringwebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringwebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebProjectApplication.class, args);
+	}
+
+}
